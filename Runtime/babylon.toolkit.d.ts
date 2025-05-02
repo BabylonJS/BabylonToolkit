775a41d7f091057a0c65363402b8aa9b7c6f346e
MackeyK24 <MackeyK24@gmail.com>
2025-05-01 18:53:34 -1000

    Babylon Toolkit 8.5.0

--- Runtime/babylon.toolkit.d.ts
+++ Runtime/babylon.toolkit.d.ts
@@ -6,7 +6,7 @@ declare namespace TOOLKIT {
     * @class SceneManager - All rights reserved (c) 2024 Mackey Kinard
     */
     class SceneManager {
-        /** Gets the toolkit framework version number (8.4.0 - R1) */
+        /** Gets the toolkit framework version number (8.5.0 - R1) */
         static get Version(): string;
         /** Gets the toolkit framework copyright notice */
         static get Copyright(): string;
@@ -34,7 +34,7 @@ declare namespace TOOLKIT {
         static SupportSRGBBuffers: boolean;
         /** The animation start mode. Defaults to NONE. */
         static AnimationStartMode: number;
-        /** When loading glTF animations, which are defined in seconds, target them to this FPS. Defaults to 30 for for new behavior. Set to 1 for old behavior */
+        /** When loading glTF animations, which are defined in seconds, target them to this FPS. Defaults to 60 for new behavior. Set to 1 for old behavior */
         static AnimationTargetFps: number;
         /** Set the default convex hull shape margin size */
         static DefaultConvexHullMargin: number;
@@ -2607,6 +2607,13 @@ declare namespace TOOLKIT {
         static MobileControlsActive: boolean;
         /** Is mobile pinch zoom tracking enabled */
         static EnablePinchZoomTracking: boolean;
+        /** Enable user input state in the scene. */
+        static EnableUserInput(engine: BABYLON.AbstractEngine, scene: BABYLON.Scene, options?: {
+            contextMenu?: boolean;
+            pointerLock?: boolean;
+            preventDefault?: boolean;
+            useCapture?: boolean;
+        }): void;
         /** Configure user input state in the scene. */
         static ConfigureUserInput(engine: BABYLON.AbstractEngine, scene: BABYLON.Scene, options?: {
             contextMenu?: boolean;
