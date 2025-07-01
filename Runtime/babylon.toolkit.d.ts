7a3c0759793dfb60918c7bb5bf84cad0bccc1c08
MackeyK24 <MackeyK24@gmail.com>
2025-07-01 12:09:17 -1000

    8.14.54 Update

--- Runtime/babylon.toolkit.d.ts
+++ Runtime/babylon.toolkit.d.ts
@@ -6,7 +6,7 @@ declare namespace TOOLKIT {
     * @class SceneManager - All rights reserved (c) 2024 Mackey Kinard
     */
     class SceneManager {
-        /** Gets the toolkit framework version number (8.14.52 - R1) */
+        /** Gets the toolkit framework version number (8.14.54 - R1) */
         static get Version(): string;
         /** Gets the toolkit framework copyright notice */
         static get Copyright(): string;
