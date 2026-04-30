644c28fe4f69be907c786322c890138bda114f04
MackeyK24 <MackeyK24@gmail.com>
2026-04-30 11:18:55 -1000

    Babylon Toolkit Version 9.1.0

--- Runtime/babylon.toolkit.d.ts
+++ Runtime/babylon.toolkit.d.ts
@@ -7,7 +7,7 @@ declare namespace TOOLKIT {
     * @class SceneManager - All rights reserved (c) 2024 Mackey Kinard
     */
     class SceneManager {
-        /** Gets the toolkit framework version string (9.0.1 - R1) */
+        /** Gets the toolkit framework version string (9.1.0 - R1) */
         static get Version(): string;
         /** Gets the toolkit framework copyright notice */
         static get Copyright(): string;
